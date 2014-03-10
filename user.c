d6e748f0cb03ddb6458b30a28b7a512d0f14c06b
ytaler <yamiltaler@gmail.com>
2014-03-10 19:09:19 -0300

    Modificacion de los puertos de IO (TRISIO): GP3 entrada, GP1 salida. Eliminacion codigo inicializacion, se encuentra todo en la seccion de interrupciones. Modificacion de puertos de interrupcion (IOC): se grego GP3 . Inicializacion transistor que comanda la fuente desactivado, GP1 = 1, transistor PNP

--- user.c
+++ user.c
@@ -24,20 +24,8 @@ void InitApp(void)
     ADCON0 = 0;     // ADC apagado
     T1CON = 0;      // Apgamos el timer 1
     ANSEL = 0;      // AD Apagado
-    TRISIO = 0x1F;  // Configuramos GP5 y GP1 como salida, el resto como entrada - 0001-1101
+    TRISIO = 0x1D;  // Configuramos GP5 y GP1 como salida, el resto como entrada - 0001-1101
+    GP1 = 1;        // Comanda fuente secundaria, transistor PNP
     WPU = 0;        // Deshabilitamos las resistencias pull-up
-    IOC = 0x1C;     // Habilitamos interrupciones por cambio de GP4 y GP2 - 0001-1100
-    if(GP2){        // Verificamos si esta habilitado cuando prendemos el micro
-        ADRESL = 1;         // Circuito encendido. Este registro es para otra cosa
-                            // pero lo usamos para comunicarnos entre funciones
-        OPTION_REG = 0x80;  // Configuramos el timer con flanco descendente
-        INTCON = 0x88;      // Habilitamos interrupciones global e interrupcion por cambio de estado en GP2 y GP4
-                            // GIE=1, GPIE=1 - 1000-1000
-    }
-    else{
-        ADRESL = 0;         // Circuito apagado
-        OPTION_REG = 0xC0;  // Configuramos el timer para funcionar con flanco ascendente
-        INTCON = 0x90;      // Habilitamos interrupciones global e interrupcion extrena por el pin GP2
-                            // GIE=1, INTE=1 - 1001-0000
-    }
+    IOC = 0x1C;     // Habilitamos interrupciones por cambio de GP2, GP3 y GP4 - 0001-1100
 }
