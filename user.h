ae8e089c92c6792a9f9a370097c9b345eb80f3e0
ytaler <yamiltaler@gmail.com>
2014-03-11 21:14:41 -0300

    Agregado de funciones para apagar y encender la fuente de alimentacion secundaria controlada por un transistor PNP

--- user.h
+++ user.h
@@ -6,7 +6,9 @@
 /* User Function Prototypes                                                   */
 /******************************************************************************/
 
-#define Enciende_Luz() GP5 = 0x01;
-#define Apaga_Luz() GP5 = 0x00;
+#define Enciende_Luz() GP5 = 1;
+#define Apaga_Luz() GP5 = 0;
+#define Enciende_Fuente() GP1 = 0;
+#define Apaga_Fuente() GP1 = 1;
 
 void InitApp(void);         // Inicializacion de I/O y perifericos
